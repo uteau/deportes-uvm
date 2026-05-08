6049bed1bad107f6e7ca1bd1d2ed9bd03623c55a
uteau <guerrauteaub@gmail.com>
2026-05-08 15:28:40 -0400

    fix: renombrar variable id -> sub en jwt.strategy

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -19,7 +19,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
     validate(payload: { sub: string; email: string, rol: string }) {
         return {
-            id: payload.sub,
+            sub: payload.sub,
             email: payload.email,
             rol: payload.rol,
         };
