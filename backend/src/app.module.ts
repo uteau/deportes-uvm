85ad42d3cc01581173eac57dec5822d9b6056dd6
uteau <guerrauteaub@gmail.com>
2026-05-10 01:41:24 -0400

    chore: modulo partidos incorporado en app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { EventosModule } from './eventos/eventos.module';
+import { PartidosModule } from './partidos/partidos.module';
 
 @Module({
   imports: [
     PrismaModule,
     AuthModule,
     EventosModule,
+    PartidosModule,
   ],
 })
 export class AppModule {}
